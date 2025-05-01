e45571fa45c30ca873f0884acb2af010b1328a35
Anay0305 <anayg0305@gmail.com>
2025-05-02 04:24:39 +0530

    s

--- WebApp/settings.py
+++ WebApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8!dou^tct9*j8sztv+*8wx9=rqastcz###!q&i913(0@+v+h-+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
